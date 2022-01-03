2b4de08bff89ddb963dc4398d95589576fb342ca
vidhik2002 <vidhik2002@gmail.com>
2022-01-03 16:18:27 +0530

    feat: Initial setup

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+const cors = require('cors');
+
+// require('./models/dbInit');
+
+app.use(cors())
+
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// const authMiddleware = require('./middleware/authorize')
+
+// const testRoute = require('./routes/test')
+
+// app.use('/test', testRoute);
+
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('server started');
+});
