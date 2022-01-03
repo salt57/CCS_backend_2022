64bac2a297afe7529b57b03998ec6ea814a88f9f
vidhik2002 <vidhik2002@gmail.com>
2022-01-03 19:02:54 +0530

    feat: Loggers setup

---
+++ tools/error_codes.js
@@ -0,0 +1,8 @@
+const error_codes = {
+  E0: "undefined error occured",
+  E1: "unexpected values recieved",
+  E2: "token validation failed",
+  E3: "values not found in database",
+};
+
+module.exports = { error_codes};
