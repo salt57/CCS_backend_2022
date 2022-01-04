d84ca383b2bc7c0576085bbd0f638ee047877b25
vidhik2002 <vidhik2002@gmail.com>
2022-01-04 15:24:57 +0530

    feat: AutoSave Route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const rateLimit = require("express-rate-limit");
 const cors = require('cors');
 
 require("dotenv/config");
@@ -11,13 +12,20 @@ app.use(cors())
 app.set('trust proxy', true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const authMiddleware = require('./middleware/authorize')
 
+const authMiddleware = require('./middleware/authorize')
 const testRoute = require('./routes/test')
 const submitRoute = require('./routes/submit')
+const autosaveRoute = require('./routes/autosave')
+
+const apiLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  max: 10,
+});
 
 app.use('/test', testRoute);
 app.use('/submit', submitRoute);
+app.use('/autosave',apiLimiter, autosaveRoute);
 
 
 app.use((error, req, res, next) => {
